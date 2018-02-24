ef0d6a4a62cb2ec37ed4103d1af12506ec001aac
wendy yu <yu.wendyw@gmail.com>
2018-02-24 20:45:17 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,54 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kit kat', 'skittles'];
+
+function addElementToBeginningOfArray() {
+  const addElementToBeginningOfArray = [1]
+  const addElementToBeginningOfArray2 = ['foo', ...addElementToBeginningOfArray]
+  return addElementToBeginningOfArray2
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+  var destructivelyAddElementToBeginningOfArray = [1]
+  destructivelyAddElementToBeginningOfArray.unshift('foo')
+  return destructivelyAddElementToBeginningOfArray
+}
+
+function addElementToEndOfArray() {
+  const addElementToEndOfArray= [1]
+  const addElementToEndOfArray2 = [...addElementToEndOfArray, 'foo']
+  return addElementToEndOfArray2
+}
+
+function destructivelyAddElementToEndOfArray() {
+  var destructivelyAddElementToEndOfArray = [1];
+  destructivelyAddElementToEndOfArray.push('foo');
+  return destructivelyAddElementToEndOfArray
+}
+
+function accessElementInArray () {
+  var accessElementInArray = [3];
+  return (accessElementInArray[0]);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray () {
+  var destructivelyRemoveElementFromBeginningOfArray = [1,2,3];
+  destructivelyRemoveElementFromBeginningOfArray.shift()
+  return destructivelyRemoveElementFromBeginningOfArray
+}
+
+function removeElementFromBeginningOfArray () {
+  var removeElementFromBeginningOfArray = [1,2,3];
+  removeElementFromBeginningOfArray.slice(1)
+  return removeElementFromBeginningOfArray.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray () {
+  var destructivelyRemoveElementFromEndOfArray = [1,2,3];
+  destructivelyRemoveElementFromEndOfArray.pop()
+  return destructivelyRemoveElementFromEndOfArray
+}
+
+function removeElementFromEndOfArray () {
+  var removeElementFromEndOfArray = [1,2,3];
+  removeElementFromEndOfArray.slice(0, removeElementFromEndOfArray.length - 1)
+  return removeElementFromEndOfArray.slice(0, removeElementFromEndOfArray.length - 1)
+}
